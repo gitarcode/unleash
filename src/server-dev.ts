560655f79968c96ac6fded1018291292f4c2e178
Gitar <noreply@gitar.co>
2024-08-19 17:20:31 +0000

    [Gitar] Updating TypeScript files

--- src/server-dev.ts
+++ src/server-dev.ts
@@ -44,7 +44,6 @@ process.nextTick(async () => {
                         celebrateUnleash: true,
                         featureSearchFeedbackPosting: true,
                         killInsightsUI: false,
-                        userAccessUIEnabled: true,
                         outdatedSdksBanner: true,
                         disableShowContextFieldSelectionValues: false,
                         projectOverviewRefactorFeedback: true,
