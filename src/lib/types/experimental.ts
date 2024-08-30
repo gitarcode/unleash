b0fded5c2d66ea2842f21314b0c2c041cca2bd92
Gitar <noreply@gitar.co>
2024-08-30 21:26:49 +0000

    [Gitar] Updating TypeScript files

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -26,7 +26,6 @@ export type IFlagKey =
     | 'filterInvalidClientMetrics'
     | 'disableMetrics'
     | 'stripHeadersOnAPI'
-    | 'signals'
     | 'automatedActions'
     | 'celebrateUnleash'
     | 'featureSearchFeedback'
