5c1002df0806e9830769ee735be5f0318f098f6c
Gitar <noreply@gitar.co>
2024-08-30 21:18:33 +0000

    [Gitar] Updating TypeScript files

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -12,7 +12,6 @@ export type IFlagKey =
     | 'embedProxyFrontend'
     | 'responseTimeWithAppNameKillSwitch'
     | 'maintenanceMode'
-    | 'messageBanner'
     | 'featuresExportImport'
     | 'caseInsensitiveInOperators'
     | 'strictSchemaValidation'
