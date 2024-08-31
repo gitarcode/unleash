ab26350c595bc2cd3da4c413970675c773589c1e
Gitar <noreply@gitar.co>
2024-08-31 00:56:30 +0000

    [Gitar] Updating TSX files

--- frontend/src/component/project/ProjectCard/ProjectCardFooter/ProjectCardFooter.tsx
+++ frontend/src/component/project/ProjectCard/ProjectCardFooter/ProjectCardFooter.tsx
@@ -6,10 +6,6 @@ import {
     ProjectOwners as LegacyProjectOwners,
 } from '../LegacyProjectOwners/LegacyProjectOwners';
 import { ProjectOwners } from './ProjectOwners/ProjectOwners';
-import { useUiFlag } from 'hooks/useUiFlag';
-import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
-import { ProjectMembers } from './ProjectMembers/ProjectMembers';
-import { DEFAULT_PROJECT_ID } from 'hooks/api/getters/useDefaultProject/useDefaultProjectId';
 
 interface IProjectCardFooterProps {
     id?: string;
@@ -34,29 +30,14 @@ const StyledFooter = styled(Box)<{ disabled: boolean }>(
 );
 
 export const ProjectCardFooter: FC<IProjectCardFooterProps> = ({
-    id,
     children,
     owners,
     disabled = false,
-    memberCount,
 }) => {
-    const projectListImprovementsEnabled = useUiFlag('projectListImprovements');
-
     return (
         <StyledFooter disabled={disabled}>
-            <ConditionallyRender
-                condition={Boolean(projectListImprovementsEnabled)}
-                show={<ProjectOwners owners={owners} />}
-                elseShow={<LegacyProjectOwners owners={owners} />}
-            />
-            <ConditionallyRender
-                condition={
-                    Boolean(projectListImprovementsEnabled) &&
-                    id !== DEFAULT_PROJECT_ID
-                }
-                show={<ProjectMembers count={memberCount} members={[]} />}
-                elseShow={children}
-            />
+            <LegacyProjectOwners owners={owners} />
+            {children}
         </StyledFooter>
     );
 };
