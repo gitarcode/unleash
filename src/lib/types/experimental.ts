b1c83dedeb923bab73b3645df7688f09d089d679
Gitar <noreply@gitar.co>
2024-08-31 00:55:41 +0000

    [Gitar] Updating TypeScript files

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -50,7 +50,6 @@ export type IFlagKey =
     | 'displayEdgeBanner'
     | 'disableShowContextFieldSelectionValues'
     | 'projectOverviewRefactorFeedback'
-    | 'manyStrategiesPagination'
     | 'enableLegacyVariants'
     | 'navigationSidebar'
     | 'anonymizeProjectOwners'
@@ -253,10 +252,6 @@ const flags: IFlags = {
     ),
     projectOverviewRefactorFeedback: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_PROJECT_OVERVIEW_REFACTOR_FEEDBACK,
-        false,
-    ),
-    manyStrategiesPagination: parseEnvVarBoolean(
-        process.env.UNLEASH_EXPERIMENTAL_MANY_STRATEGIES_PAGINATION,
         false,
     ),
     enableLegacyVariants: parseEnvVarBoolean(
