d68c5a3535bcc2f35f77c8b028b4b7daa22ed3d3
Gitar <noreply@gitar.co>
2024-08-31 00:53:28 +0000

    [Gitar] Updating TypeScript files

--- src/lib/features/metrics/instance/metrics.ts
+++ src/lib/features/metrics/instance/metrics.ts
@@ -1,7 +1,6 @@
 import type { Response } from 'express';
 import Controller from '../../../routes/controller';
 import {
-    CLIENT_METRICS,
     type IFlagResolver,
     type IUnleashConfig,
     type IUnleashServices,
@@ -20,8 +19,6 @@ import {
 import rateLimit from 'express-rate-limit';
 import { minutesToMilliseconds } from 'date-fns';
 import type { BulkMetricsSchema } from '../../../openapi/spec/bulk-metrics-schema';
-import { clientMetricsEnvBulkSchema } from '../shared/schema';
-import type { IClientMetricsEnv } from '../client-metrics/client-metrics-store-v2-type';
 
 export default class ClientMetricsController extends Controller {
     logger: Logger;
@@ -106,70 +103,13 @@ export default class ClientMetricsController extends Controller {
     }
 
     async registerMetrics(req: IAuthRequest, res: Response): Promise<void> {
-        if (this.config.flagResolver.isEnabled('disableMetrics')) {
-            res.status(204).end();
-        } else {
-            try {
-                const { body: data, ip: clientIp, user } = req;
-                data.environment = this.metricsV2.resolveMetricsEnvironment(
-                    user,
-                    data,
-                );
-                await this.clientInstanceService.registerInstance(
-                    data,
-                    clientIp,
-                );
-
-                await this.metricsV2.registerClientMetrics(data, clientIp);
-                res.getHeaderNames().forEach((header) =>
-                    res.removeHeader(header),
-                );
-                res.status(202).end();
-            } catch (e) {
-                res.status(400).end();
-            }
-        }
+        res.status(204).end();
     }
 
     async bulkMetrics(
         req: IAuthRequest<void, void, BulkMetricsSchema>,
         res: Response<void>,
     ): Promise<void> {
-        if (this.config.flagResolver.isEnabled('disableMetrics')) {
-            res.status(204).end();
-        } else {
-            const { body, ip: clientIp } = req;
-            const { metrics, applications } = body;
-            try {
-                const promises: Promise<void>[] = [];
-                for (const app of applications) {
-                    promises.push(
-                        this.clientInstanceService.registerClient(
-                            app,
-                            clientIp,
-                        ),
-                    );
-                }
-                if (metrics && metrics.length > 0) {
-                    const data: IClientMetricsEnv[] =
-                        await clientMetricsEnvBulkSchema.validateAsync(metrics);
-                    const { user } = req;
-                    const acceptedEnvironment =
-                        this.metricsV2.resolveUserEnvironment(user);
-                    const filteredData = data.filter(
-                        (metric) => metric.environment === acceptedEnvironment,
-                    );
-                    promises.push(
-                        this.metricsV2.registerBulkMetrics(filteredData),
-                    );
-                    this.config.eventBus.emit(CLIENT_METRICS, data);
-                }
-                await Promise.all(promises);
-
-                res.status(202).end();
-            } catch (e) {
-                res.status(400).end();
-            }
-        }
+        res.status(204).end();
     }
 }
