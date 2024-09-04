8b968c60dc6bd6c8b2de80d147d6f7faa10cc317
Gitar <noreply@gitar.co>
2024-09-04 04:37:24 +0000

    [Gitar] Updating TypeScript files

--- src/lib/features/dependent-features/dependent.features.e2e.test.ts
+++ src/lib/features/dependent-features/dependent.features.e2e.test.ts
@@ -25,7 +25,6 @@ beforeAll(async () => {
         {
             experimental: {
                 flags: {
-                    strictSchemaValidation: true,
                     enableLegacyVariants: true,
                 },
             },
