1de3c19826315418e22533aed74e0dc60d7fd687
Gitar <noreply@gitar.co>
2024-09-11 23:22:37 +0000

    [Gitar] Updating TypeScript files

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -28,7 +28,6 @@ export type IFlagKey =
     | 'stripHeadersOnAPI'
     | 'signals'
     | 'automatedActions'
-    | 'celebrateUnleash'
     | 'featureSearchFeedback'
     | 'featureSearchFeedbackPosting'
     | 'edgeBulkMetrics'
@@ -143,10 +142,6 @@ const flags: IFlags = {
     ),
     automatedActions: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_AUTOMATED_ACTIONS,
-        false,
-    ),
-    celebrateUnleash: parseEnvVarBoolean(
-        process.env.UNLEASH_EXPERIMENTAL_CELEBRATE_UNLEASH,
         false,
     ),
     featureSearchFeedback: {
