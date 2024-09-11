37b6af29c846c9837c55e8223a88eec9a0f5008f
Gitar <noreply@gitar.co>
2024-09-11 23:22:03 +0000

    [Gitar] Updating TypeScript files

--- src/lib/types/experimental.ts
+++ src/lib/types/experimental.ts
@@ -33,7 +33,6 @@ export type IFlagKey =
     | 'featureSearchFeedbackPosting'
     | 'edgeBulkMetrics'
     | 'extendedUsageMetrics'
-    | 'adminTokenKillSwitch'
     | 'feedbackComments'
     | 'showInactiveUsers'
     | 'killScheduledChangeRequestCache'
@@ -176,10 +175,6 @@ const flags: IFlags = {
     ),
     extendedUsageMetrics: parseEnvVarBoolean(
         process.env.UNLEASH_EXPERIMENTAL_EXTENDED_USAGE_METRICS,
-        false,
-    ),
-    adminTokenKillSwitch: parseEnvVarBoolean(
-        process.env.UNLEASH_EXPERIMENTAL_ADMIN_TOKEN_KILL_SWITCH,
         false,
     ),
     outdatedSdksBanner: parseEnvVarBoolean(
