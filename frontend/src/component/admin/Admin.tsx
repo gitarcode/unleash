848b66ae6a79e83c801eb67abd1fe4a7f138dcbe
Gitar <noreply@gitar.co>
2024-09-11 23:25:26 +0000

    [Gitar] Updating TSX files

--- frontend/src/component/admin/Admin.tsx
+++ frontend/src/component/admin/Admin.tsx
@@ -17,46 +17,32 @@ import { InviteLink } from './users/InviteLink/InviteLink';
 import UsersAdmin from './users/UsersAdmin';
 import NotFound from 'component/common/NotFound/NotFound';
 import { AdminIndex } from './AdminIndex';
-import { AdminTabsMenu } from './menu/AdminTabsMenu';
 import { Banners } from './banners/Banners';
 import { License } from './license/License';
-import { useUiFlag } from 'hooks/useUiFlag';
-import { ConditionallyRender } from 'component/common/ConditionallyRender/ConditionallyRender';
 
 export const Admin = () => {
-    const sidebarNavigationEnabled = useUiFlag('navigationSidebar');
-
     return (
-        <>
-            <ConditionallyRender
-                condition={!sidebarNavigationEnabled}
-                show={<AdminTabsMenu />}
-            />
-            <Routes>
-                <Route index element={<AdminIndex />} />
-                <Route path='users/*' element={<UsersAdmin />} />
-                <Route path='api' element={<ApiTokenPage />} />
-                <Route path='api/create-token' element={<CreateApiToken />} />
-                <Route path='service-accounts' element={<ServiceAccounts />} />
-                <Route path='create-user' element={<CreateUser />} />
-                <Route path='invite-link' element={<InviteLink />} />
-                <Route path='groups/*' element={<GroupsAdmin />} />
-                <Route path='roles/*' element={<Roles />} />
-                <Route path='instance' element={<InstanceAdmin />} />
-                <Route path='network/*' element={<Network />} />
-                <Route path='maintenance' element={<MaintenanceAdmin />} />
-                <Route path='banners' element={<Banners />} />
-                <Route path='license' element={<License />} />
-                <Route path='cors' element={<CorsAdmin />} />
-                <Route path='auth' element={<AuthSettings />} />
-                <Route
-                    path='admin-invoices'
-                    element={<FlaggedBillingRedirect />}
-                />
-                <Route path='billing' element={<Billing />} />
-                <Route path='instance-privacy' element={<InstancePrivacy />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-        </>
+        <Routes>
+            <Route index element={<AdminIndex />} />
+            <Route path='users/*' element={<UsersAdmin />} />
+            <Route path='api' element={<ApiTokenPage />} />
+            <Route path='api/create-token' element={<CreateApiToken />} />
+            <Route path='service-accounts' element={<ServiceAccounts />} />
+            <Route path='create-user' element={<CreateUser />} />
+            <Route path='invite-link' element={<InviteLink />} />
+            <Route path='groups/*' element={<GroupsAdmin />} />
+            <Route path='roles/*' element={<Roles />} />
+            <Route path='instance' element={<InstanceAdmin />} />
+            <Route path='network/*' element={<Network />} />
+            <Route path='maintenance' element={<MaintenanceAdmin />} />
+            <Route path='banners' element={<Banners />} />
+            <Route path='license' element={<License />} />
+            <Route path='cors' element={<CorsAdmin />} />
+            <Route path='auth' element={<AuthSettings />} />
+            <Route path='admin-invoices' element={<FlaggedBillingRedirect />} />
+            <Route path='billing' element={<Billing />} />
+            <Route path='instance-privacy' element={<InstancePrivacy />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
     );
 };
